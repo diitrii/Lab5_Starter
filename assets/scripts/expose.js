0a00c9cc08ced2b432ce856b330652a3ca565f49
Dmitri <lakedre21@gmail.com>
2026-05-05 20:56:32 -0700

    Finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,33 +5,47 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   const hornimage = document.getElementById("horn-select");
   hornimage.addEventListener("change", addHornImage); //function calling
+  document.getElementById("volume").addEventListener("input", addAudioIcon);
+  document.querySelector("button").addEventListener("click", () => {
+    const audio = document.querySelector("audio");
+    audio.play();
+  });
 }
 
 function addHornImage() {
   const horn = document.querySelector("img");
   const hornselect = (document.getElementById("horn-select")).value;
+  const audio = document.querySelector("audio");
   if (hornselect === "air-horn") {
     horn.src = "assets/images/air-horn.svg";
+    audio.src = "assets/audio/air-horn.mp3";
   } else if (hornselect === "car-horn") {
     horn.src = "assets/images/car-horn.svg";
+    audio.src = "assets/audio/car-horn.mp3";
   } else if (hornselect === "party-horn") {
     horn.src = "assets/images/party-horn.svg";
+    audio.src = "assets/audio/party-horn.mp3";
   }
 }
 
-function addAudio() {
-  const audio = document.getElementById("volume-controls: img");
-  const audioselect = (document.getElementById("volume-controls")).value;
-  if (audio === "0") {
+function addAudioIcon() {
+  const audio = document.querySelector("audio");
+  const audioIcon = document.querySelector("#volume-controls img");
+  const audioSelectIcon = parseInt((document.getElementById("volume")).value);
 
+  audio.volume = audioSelectIcon / 100;
+
+  if (audioSelectIcon === 0) {
+    audioIcon.src = "assets/icons/volume-level-0.svg";
   }
-  else if (audio > "1" || audio < "33") {
-
+  else if (audioSelectIcon >= 1 && audioSelectIcon < 33) {
+    audioIcon.src = "assets/icons/volume-level-1.svg";
   }
-  else if (audio >= "33" || audio < "67") {
-
+  else if (audioSelectIcon >= 33 && audioSelectIcon < 67) {
+    audioIcon.src = "assets/icons/volume-level-2.svg";
   }
-  else if (audio >= "67") {
-
+  else if (audioSelectIcon >= 67) {
+    audioIcon.src = "assets/icons/volume-level-3.svg";
   }
 }
+
